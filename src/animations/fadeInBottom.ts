dc994c14a65e9e7687e8a7b0150cc409511380ef
Agustín Vega <agustinvegadev@gmail.com>
2022-02-14 12:21:07 -0300

    Fade in bottom animation added

---
+++ src/animations/fadeInBottom.ts
@@ -0,0 +1,16 @@
+// t = transition, i = initial, a = animate
+
+export const tFadeinB = {
+    type: "spring",
+    duration: 1
+}
+
+export const iFadeinB = {
+    y: 100,
+    opacity: 0
+}
+
+export const aFadeinB = {
+    y: 0,
+    opacity: 1
+}
