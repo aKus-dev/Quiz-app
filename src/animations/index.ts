94af887d71ae273b20ba6872c96473640a53936a
Agustín Vega <agustinvegadev@gmail.com>
2022-02-14 12:10:35 -0300

    Transparent buttons style in difficulty screen

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -1,3 +1,4 @@
 export { tPopup, iPopup, aPopup } from './popUp';
 export { tFadeinR, iFadeinR, aFadeinR } from './fadeInRight';
 export { tFadeinL, iFadeinL, aFadeinL } from './fadeInLeft';
+export { tFadeinB, iFadeinB, aFadeinB } from './fadeInBottom';
--- src/components/DifficultyScreen/Difficulty.tsx
+++ src/components/DifficultyScreen/Difficulty.tsx
@@ -2,8 +2,7 @@ import styles from '../../scss/DifficultyScreen/difficulty.module.scss';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { 
-  iFadeinL, aFadeinL, tFadeinL,
-  iFadeinR, aFadeinR, tFadeinR 
+  iFadeinB, aFadeinB, tFadeinB
 } from '../../animations';
 
 export const Difficulty = () => {
@@ -11,27 +10,27 @@ export const Difficulty = () => {
   return (
     <div className={styles.container}>
       <motion.div
-        initial={iFadeinL}
-        animate={aFadeinL}
-        transition={tFadeinL}
+        initial={iFadeinB}
+        animate={aFadeinB}
+        transition={{...tFadeinB, delay: 0}}
         className={styles.linkContainer}
       >
         <Link to="#" className={`${styles.btn} ${styles.btn1}`}>Easy</Link>
       </motion.div>
 
       <motion.div
-        initial={iFadeinR}
-        animate={aFadeinR}
-        transition={tFadeinR}
+        initial={iFadeinB}
+        animate={aFadeinB}
+        transition={{...tFadeinB, delay: 0.35}}
         className={styles.linkContainer}
       >
         <Link to="#" className={`${styles.btn} ${styles.btn2}`}>Normal</Link>
       </motion.div>
 
       <motion.div
-        initial={iFadeinL}
-        animate={aFadeinL}
-        transition={tFadeinL}
+        initial={iFadeinB}
+        animate={aFadeinB}
+        transition={{...tFadeinB, delay: 0.65}}
         className={styles.linkContainer}
       >
         <Link to="#" className={`${styles.btn} ${styles.btn3}`}>Hard</Link>
