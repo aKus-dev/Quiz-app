57fde5c318b51c5e8e3239bfd0fc64d4135d0db4
Agustín Vega <agustinvegadev@gmail.com>
2022-02-18 19:22:24 -0300

    Animation added to Results.tsx

--- src/components/QuizScreen/Results.tsx
+++ src/components/QuizScreen/Results.tsx
@@ -2,28 +2,36 @@ import styles from '../../scss/QuizScreen/quiz.module.scss';
 import { useContext } from 'react'
 import { QuizContext } from '../../context/QuizContext';
 import { generateData } from '../../helpers/generateData';
+import { Navigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import Faq from "react-faq-component";
-import { Navigate } from 'react-router-dom';
+import { iFadeinB, aFadeinB, tFadeinB } from '../../animations';
 
 export const Results = () => {
 
   const { questions } = useContext(QuizContext);
 
-  if(!questions) return <Navigate to="/" />
+  if (!questions) return <Navigate to="/" />
 
   const data = generateData(questions!);
 
   return (
     <div className={styles.resultsContainer}>
-      <div className={styles.answerContainer}>
+
+      <motion.div
+        initial={iFadeinB}
+        animate={aFadeinB}
+        transition={tFadeinB}
+        className={styles.answerContainer}
+      >
 
         <Faq
           data={data}
           styles={data.styles}
         />
 
-      </div>
+      </motion.div>
     </div>
   )
 }
